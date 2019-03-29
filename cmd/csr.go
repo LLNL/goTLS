415427ffa286b34b0e78e778d428ef20cec88a4f
Ian Freeman <ifreeman@llnl.gov>
2019-03-29 09:36:53 -0700

    Read $HOME or working dir config file for CSR template values.

--- cmd/csr.go
+++ cmd/csr.go
@@ -7,9 +7,15 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var email string
+var c string
+var st string
+var l string
+var o string
+var ou string
 
 // csrCmd represents the csr command
 var csrCmd = &cobra.Command{
@@ -20,12 +26,39 @@ If a private key matching the given primary hostname exists in the current
 directory it will be used, otherwise a new key will be created.`,
 	Args: cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("not implemented")
+		// load flag variables
+		c = viper.GetString("c")
+		st = viper.GetString("st")
+		l = viper.GetString("l")
+		o = viper.GetString("o")
+		ou = viper.GetString("ou")
+		email = viper.GetString("email")
+
+		fmt.Printf("C: %s\n", c)
+		fmt.Printf("ST: %s\n", st)
+		fmt.Printf("L: %s\n", l)
+		fmt.Printf("O: %s\n", o)
+		fmt.Printf("OU: %s\n", ou)
+		fmt.Printf("email: %s\n", email)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(csrCmd)
 
+	// define flags
 	csrCmd.Flags().StringVarP(&email, "email", "e", "", "Email address to submit")
+	csrCmd.Flags().StringVarP(&c, "c", "c", "", "Country field for CSR")
+	csrCmd.Flags().StringVarP(&st, "st", "", "", "State field for CSR")
+	csrCmd.Flags().StringVarP(&l, "l", "l", "", "Locality or city field for CSR")
+	csrCmd.Flags().StringVarP(&o, "o", "o", "", "Organization field for CSR")
+	csrCmd.Flags().StringVarP(&ou, "ou", "", "", "Organization Unit field for CSR")
+
+	// bind flags to conf file values (appears to be case-insensitive)
+	viper.BindPFlag("c", csrCmd.Flags().Lookup("c"))
+	viper.BindPFlag("st", csrCmd.Flags().Lookup("st"))
+	viper.BindPFlag("l", csrCmd.Flags().Lookup("l"))
+	viper.BindPFlag("o", csrCmd.Flags().Lookup("o"))
+	viper.BindPFlag("ou", csrCmd.Flags().Lookup("ou"))
+	viper.BindPFlag("email", csrCmd.Flags().Lookup("email"))
 }
--- cmd/root.go
+++ cmd/root.go
@@ -5,9 +5,9 @@ package cmd
 
 import (
 	"fmt"
-	"os"
+	"log"
 
-	//homedir "github.com/mitchellh/go-homedir"
+	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -27,11 +27,9 @@ Let's Encrypt service.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatalf("Error: %s", err)
 	}
 }
 
@@ -39,7 +37,7 @@ func init() {
 	cobra.OnInitialize(initConfig)
 
 	// persistent flags will be global for your application.
-	//rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gotls.yaml)")
+	rootCmd.PersistentFlags().StringVar(&confFile, "config", "", "config file (default is $HOME/.gotls.yaml)")
 
 	// local flags will only run when this action is called directly.
 	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
@@ -47,26 +45,22 @@ func init() {
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-	//if cfgFile != "" {
-	//	// Use config file from the flag.
-	//	viper.SetConfigFile(cfgFile)
-	//} else {
-	//	// Find home directory.
-	//	home, err := homedir.Dir()
-	//	if err != nil {
-	//		fmt.Println(err)
-	//		os.Exit(1)
-	//	}
-	//
-	//	// Search config in home directory with name ".gotls" (without extension).
-	//	viper.AddConfigPath(home)
-	//	viper.SetConfigName(".gotls")
-	//}
+	if confFile != "" {
+		viper.SetConfigFile(confFile)
+	} else {
+		home, err := homedir.Dir()
+		if err != nil {
+			log.Fatalf("Error: %s", err)
+		}
+		viper.AddConfigPath(home)
+		viper.AddConfigPath(".") // override config from the working directory
+		viper.SetConfigName(".gotls") // do not set the extension; viper will try all known types
+	}
 
 	viper.AutomaticEnv() // read in environment variables that match
 
-	//// If a config file is found, read it in.
-	//if err := viper.ReadInConfig(); err == nil {
-	//	fmt.Println("Using config file:", viper.ConfigFileUsed())
-	//}
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Printf("using config file: %s\n", viper.ConfigFileUsed())
+	}
 }
